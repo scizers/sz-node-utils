65fa6b5ec29bc889d79399002b64720ff150f0da
Ishaan's Macbook Air <admin@scizers.com>
2020-02-02 23:07:44 +0530

    added node utils

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import table, { TableFilterQuery as tx, TableFilterQueryWithAggregate as tagx } from './utils/table.js'
 import { handleMediaUpload as hmu, handleSingleMediaUpload as hsmu, handleMediaResponse as hmr } from './utils/utils.js'
-import { TORMQuery } from './utils/typeORM'
+import { TORMQuery : tmr } from './utils/typeORM'
 
 export const TableFilterQuery = tx
 export const TableFilterQueryWithAggregate = tagx
 export const handleMediaUpload = hmu
 export const handleSingleMediaUpload = hsmu
 export const handleMediaResponse = hmr
-export const TORMQuery
+export const TORMQuery = tmr
 
 export default {
   TORMQuery,
